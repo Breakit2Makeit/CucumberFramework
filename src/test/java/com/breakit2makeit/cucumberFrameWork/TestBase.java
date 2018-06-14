7499f51d9019506b3760130f3221b22b65646061
johnskpaul <jk.paul@pitt.edu>
2018-06-14 15:43:00 -0400

    Updated by @johnskpaul

--- src/test/java/com/breakit2makeit/cucumberFrameWork/TestBase.java
+++ src/test/java/com/breakit2makeit/cucumberFrameWork/TestBase.java
@@ -1,17 +1,14 @@
 package com.breakit2makeit.cucumberFrameWork;
 
+import java.util.concurrent.TimeUnit;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.annotations.AfterClass;
 import org.testng.annotations.AfterMethod;
-import org.testng.annotations.AfterSuite;
-import org.testng.annotations.AfterTest;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
-import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 /*
@@ -28,20 +25,27 @@ public class TestBase {
 	
 	  @BeforeMethod
 	  public void beforeMethod() {
+		  System.setProperty("webdriver.gecko.driver", "C:\\Program Files\\Apache\\geckodriver.exe");
 		  driver = new FirefoxDriver();
 		  baseURL = "https://www.breakit2makeit.org/";
+		  driver.manage().window().maximize();
+		  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
 		  
 	  }
 	
   @Test
-  public void testBase() {
+  public void testBase() throws Exception {
 	  
 	  driver.get(baseURL);
+	  driver.findElement(By.xpath("//div[@name='Message']//textarea")).sendKeys("Test");
+
+	  
   }
 
 
   @AfterMethod
   public void afterMethod() {
+//	  driver.quit();
   }
 
   /*@BeforeClass
